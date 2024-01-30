3d3882c29b2bed2112aee7710aab134e0c2383c8
Coeurxx <2543046796@qq.com>
2024-01-30 21:58:39 +0800

    feat: init文件预览功能

---
+++ src/components/Preview/Preview.tsx
@@ -0,0 +1,5 @@
+// 整合各种预览组件
+const Preview = () => {
+  return <div>123</div>
+}
+export default Preview
---
+++ src/components/Preview/PreviewVideo.tsx
@@ -0,0 +1,4 @@
+const PreviewVideo = () => {
+  return <div>123</div>
+}
+export default PreviewVideo
